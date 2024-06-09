b341f8bcab16e5849c4bc8f8cdd22d0f6dde091e
muditmathurr <muditmathur05@gmail.com>
2024-06-09 18:32:07 +0530

    icon link

--- Homd.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) { // Adjust 50 to the desired scroll position
-      navbar.classList.add('navbar-scrolled');
-    } else {
-      navbar.classList.remove('navbar-scrolled');
-    }
-  });
-  
